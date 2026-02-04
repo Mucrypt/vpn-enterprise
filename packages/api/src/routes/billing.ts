cf3ec2e46639a29f9f73f3cde31146c4da5e919c
mucrypt <romeomukulah@gmail.com>
2026-02-04 18:15:09 +0100

    update apiVersion

--- packages/api/src/routes/billing.ts
+++ packages/api/src/routes/billing.ts
@@ -16,7 +16,7 @@ const router = Router()
 // Initialize Stripe
 const stripe = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2026-01-28.clover',
+      apiVersion: '2025-02-24.acacia',
     })
   : null
 
